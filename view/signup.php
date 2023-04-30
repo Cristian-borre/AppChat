502ba7727c0c8aab7721e0ce2f66cb420a4c9080
Cristian-borre <crstnborre01@gmail.com>
2023-04-30 12:14:20 -0500

    commit

--- view/signup.php
+++ view/signup.php
@@ -15,36 +15,36 @@
 <body class="m-0 row justify-content-center align-items-center">
     <div class="signup col-auto h-auto shadow-lg p-5 rounded-5">
         <div>
-            <form action="./home">
+            <form>
                 <div class="head">
                     <h1 class="title text-center m-0 mb-3 p-0">Register</h1>
                 </div>
                 <div class="body">
                     <div class="row g-2 mb-4 mt-4">
                         <div class="col form-floating">
-                            <input type="text" class="form-control" placeholder="Name" id="Inputname">
+                            <input type="text" class="form-control" placeholder="Name" name="name" id="Inputname">
                             <label for="Inputname">Name</label>
                         </div>
                         <div class="col form-floating">
-                            <input type="text" class="form-control" placeholder="Last name" id="InputLastname">
+                            <input type="text" class="form-control" placeholder="Last name" name="lastname" id="InputLastname">
                             <label for="InputLastname">Last name</label>
                         </div>
                     </div>
                     <div class="form-floating mb-4">
-                        <input type="text" class="form-control" placeholder="Username" id="InputUsername">
+                        <input type="text" class="form-control" placeholder="Username" name="username" id="InputUsername">
                         <label for="InputUsername">Username</label>
                     </div>
                     <div class="form-floating mb-4">
-                        <input type="password" class="form-control" placeholder="Password" id="InputPassword">
+                        <input type="password" class="form-control" placeholder="Password" name="password" id="InputPassword">
                         <label for="InputPassword">Password</label>
                     </div>
                     <div class="mb-4">
                         <label for="InputPicture" class="text-start form-label fs-5">Picture</label>
-                        <input type="file" class="form-control" placeholder="Picture" id="InputPicture">
+                        <input type="file" class="form-control" placeholder="Picture" name="img" id="InputPicture">
                     </div>
                 </div>
                 <div class="footer text-center">
-                    <button type="submit" class=" btn btn-dark mb-3 fs-5 w-75 mt-3" >Sign Up</button>
+                    <button type="button" class=" btn btn-dark mb-3 fs-5 w-75 mt-3" onclick="signup()">Sign Up</button>
                     <p>Ya tienes cuenta?? <a href="./login">SignIn now</a></p>
                 </div>
             </form>
